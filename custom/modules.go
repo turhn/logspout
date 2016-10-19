d7ce078d86f75d37e20aac5fab470316de920cc8
Turhan Coskun <turhancoskun@gmail.com>
2016-10-19 20:11:25 +0300

    Logstash support

--- custom/modules.go
+++ custom/modules.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "github.com/looplab/logspout-logstash"
 	_ "github.com/gliderlabs/logspout/adapters/syslog"
-	_ "github.com/gliderlabs/logspout/transports/tcp"
+	_ "github.com/gliderlabs/logspout/transports/udp"
 )
